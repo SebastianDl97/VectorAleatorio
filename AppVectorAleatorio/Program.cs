78f93b5c5f1cd00b13cf5aa1d3bc5cc33887ec27
Sebastiam Delgado <sebaseldemente9443@outlook.com>
2018-12-17 09:00:35 -0500

    Ejercicio en clase

--- AppVectorAleatorio/Program.cs
+++ AppVectorAleatorio/Program.cs
@@ -8,8 +8,28 @@ namespace AppVectorAleatorio
 {
     class Program
     {
+        const int MAX = 5000;
         static void Main(string[] args)
         {
+            int[] x = new int[MAX];
+            leerVector(x);
+            Console.WriteLine("\n elemento del vector");
+            imprimirVector(x);
+
+        }
+        public static void leerVector(int[]x)
+        {
+            for(int i=0;i<MAX;i++)
+            {
+                var randomNumero = new Random().Next(1, 5000);
+                x[i] = randomNumero++;
+            }
+        }
+        public static void imprimirVector(int[]x)
+        {
+            for (int i = 0; i < MAX; i++)
+                Console.WriteLine("Elemento {0} = {1}", i + 1, x[i]);
+            Console.ReadLine();
         }
     }
 }
